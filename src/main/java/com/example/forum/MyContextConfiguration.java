49badaa25ebdda824ea3669255fc5516942b8985
gen1ral <ainolhuawei@gmail.com>
2023-08-03 15:16:00 +0500

    make database.properties

--- src/main/java/com/example/forum/MyContextConfiguration.java
+++ src/main/java/com/example/forum/MyContextConfiguration.java
@@ -4,21 +4,31 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
+@PropertySource("classpath:database.properties")
 public class MyContextConfiguration {
+    private final Environment environment;
+
+    public MyContextConfiguration(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public DataSource dataSource(){
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/Forum");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("021111");
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("DB.driver")));
+        dataSource.setUrl(environment.getProperty("DB.url"));
+        dataSource.setUsername(environment.getProperty("DB.username"));
+        dataSource.setPassword(environment.getProperty("DB.password"));
 
         return dataSource;
     }
